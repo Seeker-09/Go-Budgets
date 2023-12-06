326104be3c81b45f1ea5198ca7a7c4cb671dd155
David Mundt <54086931+Seeker-09@users.noreply.github.com>
2023-12-05 20:44:11 -0500

    create budget json file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Seeker-09/gobudgets
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	budget := Budget{
+		Name:   "test",
+		Amount: 100.00,
+	}
+
+	createBudget(budget)
+}
+
+type Budget struct {
+	Name   string  `json:"name"`
+	Amount float64 `json:"amount"`
+}
+
+func createBudget(budget Budget) error {
+	budgetFile, err := os.Create("budgets.json")
+	if err != nil {
+		fmt.Println("Error creating file: ", err)
+		return nil // TODO: change this
+	}
+	defer budgetFile.Close()
+
+	jsonEncoder := json.NewEncoder(budgetFile)
+
+	err = jsonEncoder.Encode(budget)
+	if err != nil {
+		fmt.Println("Error encoding JSON: ", err)
+		return nil // TODO: change this
+	}
+
+	fmt.Println("Data written to JSON successfully")
+	return nil
+}
